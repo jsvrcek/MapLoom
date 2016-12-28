d112f590bf47e7ca7353e36ae3e093cc53abe50b
Jason Rydberg <jrydberg@boundlessgeo.com>
2016-12-28 15:41:57 -0600

    Setting "building" fields to readonly

--- src/common/featuremanager/FeatureManagerService.js
+++ src/common/featuremanager/FeatureManagerService.js
@@ -663,7 +663,7 @@
 
         goog.array.forEach(properties, function(property, index) {
           // For the "bikepath" and "building" layers, set the "primary key" to a UUID
-          if (selectedLayer_.get('metadata').name.includes('bikepath') || selectedLayer_.get('metadata').includes('building')) {// &&
+          if (selectedLayer_.get('metadata').name.includes('bikepath') || selectedLayer_.get('metadata').name.includes('building')) {// &&
             if (property[0] === 'gid') {
               property[1] = generatedUuid;
             }
